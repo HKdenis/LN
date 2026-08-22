72ecf0ef5555ff616308c5a2218e530f2e33c5f6
HKdenis <kayanjadenish@gmail.com>
2026-08-22 15:59:46 +0300

    Update LN.py

--- LN.py
+++ LN.py
@@ -78,10 +78,10 @@ def login():
     """, unsafe_allow_html=True)
 
     # Use a neat vertical column centering trick for the interactive widget element
-    col1, col2, col3 = st.columns([1, 1.8, 1])
+    col1, col2, col3 = st.columns([1, 1.5, 1])
     with col2:
         with st.form("login_form", clear_on_submit=False):
-            password_input = st.text_input("Application Password", type="password", help="Enter authorization key")
+            password_input = st.text_input("System Password", type="password", help="Enter authorization key")
             submit_button = st.form_submit_button("Log In", use_container_width=False)
             
             if submit_button:
